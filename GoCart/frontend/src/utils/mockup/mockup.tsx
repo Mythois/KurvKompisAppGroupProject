be7f32ac4ff4445467d656890a14a72d70c7e3e5
Runarannem <runarannem@gmail.com>
2023-10-05 22:00:40 +0200

    [#3] feat: Make mockup data

---
+++ GoCart/frontend/src/utils/mockup/mockup.tsx
@@ -0,0 +1,20 @@
+// dataService.ts
+import rawData from './mockupData.json'; // Use the correct path to your JSON file
+
+// Define the structure of a food item
+interface Food {
+  name: string;
+  calories?: string; // Optional properties for additional information
+  carbohydrates?: string;
+  fiber?: string;
+  vitaminC?: string;
+  fat?: string;
+  protein?: string;
+}
+
+// Function to retrieve the food data
+function getFoodData(): { [key: string]: Food[] } {
+  return rawData.foods;
+}
+
+export { getFoodData };
