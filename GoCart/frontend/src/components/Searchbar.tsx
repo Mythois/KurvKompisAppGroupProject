a41e6af98c8d5038cbdfe5ad1a374f8c9156fad6
Runarannem <runarannem@gmail.com>
2023-10-06 01:06:55 +0200

    [#25] feat: Implement Searchbar component

--- GoCart/frontend/src/components/Searchbar.tsx
+++ GoCart/frontend/src/components/Searchbar.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Searchbar = () => {
+interface SearchbarProps {
+  onFilter: (value: string) => void;
+}
+
+export const Searchbar: React.FC<SearchbarProps> = ({ onFilter }) => {
+  const [filter, setFilter] = useState("");
+
+  const handleFilterChange = (value: string) => {
+    setFilter(value);
+    onFilter(value);
+  };
+
   return (
     <div>
-      This is a default component.
+      <input
+        type="text"
+        placeholder="Search..."
+        value={filter}
+        onChange={(e) => handleFilterChange(e.target.value)}
+        className="w-full p-2 rounded-md border border-gray-300 focus:outline-none focus:border-black"
+      />
     </div>
   );
 };
-
-export default Searchbar;
