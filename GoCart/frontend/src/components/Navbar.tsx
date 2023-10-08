0266e93e245c5686919d5c2d0c400c18402e30be
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-10-08 19:56:07 +0200

    [#29] style: Add underline on hover

--- GoCart/frontend/src/components/Navbar.tsx
+++ GoCart/frontend/src/components/Navbar.tsx
@@ -18,8 +18,8 @@ const Navbar: React.FC<NavbarProps> = ({ title }) => {
     <nav className='h-24 w-screen bg-green-700 py-6 px-5 sm:grid grid-flow-col grid-cols-5'>
       <h1 className='text-center sm:text-left text-5xl text-white font-extralight col-span-4'>{title}</h1>
       <div className='hidden sm:flex text-white justify-center gap-10'>
-        <button className=''>Home</button>
-        <button className=''>Browse</button>
+        <button className='hover:underline'>Home</button>
+        <button className='hover:underline'>Browse</button>
       </div>
     </nav>
     <footer className='sm:hidden absolute bottom-0 w-screen h-14 bg-green-700 grid grid-flow-col justify-items-center'>
