9432198e070f5cb7e5f11c051682312148fda488
Åshild Føllesdal <amfolle@stud.ntnu.no>
2023-10-08 20:40:08 +0200

    [#35] feat: implement homepage
    whith nesesary changes in main for routing
    also added hook for getting window width and hight

--- GoCart/frontend/src/pages/Home.tsx
+++ GoCart/frontend/src/pages/Home.tsx
@@ -1,1 +1,23 @@
-// "Forside" -> The site that shows a list of your shoppinglists, as well as the opportunity to create a new shoppinglist or add an item to the register
+import ListShoppingList from '../components/ListShoppingList'  
+import useWindowDimensions from '../utils/useWindowDeimensions';
+
+// <ItemList items = {countries}/>
+
+function Home() {
+  
+    const { width } = useWindowDimensions();
+
+  return (
+  
+      <div className='flex flex-row'>
+        <div className='bg-red-400 flex-1'><ListShoppingList/></div>
+      { width > 500 && 
+        <div className='bg-green-400 flex-1'>ShoppingList klone:</div>  
+        } 
+    </div>
+
+  );
+}
+
+export default Home;
+
