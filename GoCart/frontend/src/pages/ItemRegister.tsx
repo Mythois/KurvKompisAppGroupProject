62c14456bc79f177c99a94621a8de903dd2ca6c6
Runarannem <runarannem@gmail.com>
2023-10-08 20:27:08 +0200

    [#38] feat: Implement base for ItemRegister page

--- GoCart/frontend/src/pages/ItemRegister.tsx
+++ GoCart/frontend/src/pages/ItemRegister.tsx
@@ -1,1 +1,51 @@
-// "Vareregister" -> The register for every item in the database, where you get the opportunity to go to a site for adding items to the database
+// The register for every item in the database, where you get the opportunity to go to a site for adding items to the database
+
+import React, { useState, useEffect } from 'react';
+import { getFoodData, Food } from '../utils/mockup/mockup';
+import { Searchbar } from '../components/Searchbar';
+import ItemList from '../components/ItemList'; 
+import NavButton from '../components/NavButton';
+
+const ItemRegister: React.FC = () => {
+    const [filter, setFilter] = useState("");
+    const [foodData, setFoodData] = useState<Food[]>([]);
+
+    useEffect(() => {
+        // Fetch the data from mockup once when the component mounts
+        const data = getFoodData();
+        // Convert the object to an array of Food[]
+        const foodArray: Food[] = Object.values(data).flatMap((items) => items);
+        setFoodData(foodArray);
+    }, []);
+
+    // Filter the food items based on the search query
+    const filteredItems = foodData.filter((item) =>
+        item.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    // Map filtered items to objects that include all props
+    const itemPropsList = filteredItems.map((item) => ({
+        itemName: item.name,
+        increment: false, // Can be customized as needed
+        decrement: false,
+        quantity: false,
+    }));
+    
+    return (
+        <div>
+            {/* Render the Searchbar component with the filter callback */}
+            <Searchbar onFilter={(value: React.SetStateAction<string>) => setFilter(value)} />
+
+            {/* Render the ItemList component with the extracted item names */}
+            <ItemList items={itemPropsList} />
+
+            {/* Add the NavButton component in the bottom right corner */}
+            <div className="button-container flex justify-end">
+                <NavButton route="/addItemToRegister" onClick={() => {}} visual="Add item to register" />
+            </div>
+        </div>
+    );
+}
+
+export default ItemRegister;
+
