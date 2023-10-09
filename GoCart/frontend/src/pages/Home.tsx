077cdf11aff2c2cc2e9c4fb37de577cc5ea27dc6
hanneswi <hanneswi@stud.ntnu.no>
2023-10-09 11:03:55 +0200

    [#35] doc: Add comments to useWindowDimensions.tsx and rename useWindowDimensions.tsx
    Rename useWindowDeimensions.tsx to useWindowDimensions.tsx. Add comments to useWindowDimensions.tsx, Use chatGPT for file description

--- GoCart/frontend/src/pages/Home.tsx
+++ GoCart/frontend/src/pages/Home.tsx
@@ -1,5 +1,7 @@
+/* The home page of the application. */
+
 import ListShoppingList from '../components/ListShoppingList'  
-import useWindowDimensions from '../utils/useWindowDeimensions';
+import useWindowDimensions from '../utils/useWindowDimensions';
 
 // <ItemList items = {countries}/>
 
@@ -9,9 +11,9 @@ function Home() {
 
   return (
   
-      <div className='flex flex-row'>
-        <div className='bg-red-400 flex-1'><ListShoppingList/></div>
-      { width > 500 && 
+    <div className='flex flex-row'>
+      <div className='bg-red-400 flex-1'><ListShoppingList/></div>
+      { width > 500 &&                                                    // Conditional render based on the width of the screen
         <div className='bg-green-400 flex-1'>ShoppingList klone:</div>  
         } 
     </div>
