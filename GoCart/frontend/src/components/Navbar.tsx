9921ada2b0032e95bb523421ccbc397529dd9753
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-10-09 14:09:45 +0200

    [#46] bug: Fix fixed footer for mobile devices
    Closes[#46]

--- GoCart/frontend/src/components/Navbar.tsx
+++ GoCart/frontend/src/components/Navbar.tsx
@@ -15,14 +15,14 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ title }) => {
   return (
     <>
-    <nav className='h-24 w-screen bg-green-700 py-6 px-5 sm:grid grid-flow-col grid-cols-5'>
+    <nav className='fixed h-24 w-screen bg-green-700 py-6 px-5 sm:grid grid-flow-col grid-cols-5'>
       <h1 className='text-center sm:text-left text-5xl text-white font-extralight col-span-4'>{title}</h1>
       <div className='hidden sm:flex text-white justify-center gap-10'>
         <button className='hover:underline'>Home</button>
         <button className='hover:underline'>Browse</button>
       </div>
     </nav>
-    <footer className='sm:hidden absolute bottom-0 w-screen h-14 bg-green-700 grid grid-flow-col justify-items-center'>
+    <footer className='sm:hidden fixed bottom-0 w-screen h-14 bg-green-700 grid grid-flow-col justify-items-center'>
       <button><img src={HomeIcon} className='h-10 w-10' alt='Home'/></button>
       <button><img src={BrowseIcon} className='h-9 w-9' alt='Browse Items'/></button>
     </footer>
