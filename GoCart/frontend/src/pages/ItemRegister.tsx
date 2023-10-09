7581b51a6ba1a8824b865fbedb0716175ecebafe
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-10-09 18:28:48 +0200

    [#48] feat: add routing in navbar
    closes [#48]

--- GoCart/frontend/src/pages/ItemRegister.tsx
+++ GoCart/frontend/src/pages/ItemRegister.tsx
@@ -1,1 +1,11 @@
 // "Vareregister" -> The register for every item in the database, where you get the opportunity to go to a site for adding items to the database
+
+import React from 'react'
+
+function ItemRegister() {
+  return (
+    <div>ItemRegister</div>
+  )
+}
+
+export default ItemRegister
