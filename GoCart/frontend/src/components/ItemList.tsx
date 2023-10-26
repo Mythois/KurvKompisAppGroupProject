85668e17a907b043ab800ad1bef495fe23711ce0
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-10-26 17:43:43 +0200

    [#54] feat: add router and Link to ItemList
    - add router in App.tsx and div with Link element in ItemList.tsx

--- GoCart/frontend/src/components/ItemList.tsx
+++ GoCart/frontend/src/components/ItemList.tsx
@@ -1,27 +1,32 @@
 // Functional component that renders a list of 'items'
 
-import React from 'react';
-import Item from './Item';
+import React from 'react'
+import Item from './Item'
+import { Link } from 'react-router-dom'
 
 // Define the props for the ItemList component
 interface ItemListProps {
-  items: Array<{ itemName: string; increment?: boolean; decrement?: boolean; quantity?: boolean }>;
+  items: Array<{ itemName: string; increment?: boolean; decrement?: boolean; quantity?: boolean }>
 }
 
 const ItemList: React.FC<ItemListProps> = ({ items }) => {
   return (
-    <div className='bg-white p-2 rounded-2xl '>
+    <div className="bg-white p-2 rounded-2xl ">
       {items.map((item, index) => (
-        <Item
-          key={index}
-          itemName={item.itemName}
-          increment={item.increment}
-          decrement={item.decrement}
-          quantity={item.quantity}
-        />
+        <div className="hover:underline">
+          <Link to={`/ItemDetailsPage/${item.itemName}`}>
+            <Item
+              key={index}
+              itemName={item.itemName}
+              increment={item.increment}
+              decrement={item.decrement}
+              quantity={item.quantity}
+            />
+          </Link>
+        </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default ItemList;
+export default ItemList
