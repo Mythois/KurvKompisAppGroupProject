84da2b41670bc01380fff9ef2dd1068af885292f
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-10-27 17:19:58 +0200

    [#63] fix: link and route to avoid merge conflict
    Closes[#63]

--- GoCart/frontend/src/App.tsx
+++ GoCart/frontend/src/App.tsx
@@ -1,32 +1,36 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useState } from 'react';
 
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
 
-import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
-
-import Home from './pages/Home'
-import ItemRegister from './pages/ItemRegister'
-import ShoppingListPage from './pages/ShoppingListPage'
-
-
+import Home from './pages/Home';
+import ItemRegister from './pages/ItemRegister';
+import ShoppingListPage from './pages/ShoppingListPage';
+import ItemDetailsPage from './pages/ItemDetailsPage';
 
 function App() {
-
   return (
     <Fragment>
-    <Navbar title='GoCart'/>
-    <div className='pt-24 h-screen ml-16 mr-16'>
-      <Routes>
-        <Route path={import.meta.env.BASE_URL + "/Home"} element={<Home/>}/>
-        <Route path={import.meta.env.BASE_URL + "/ItemRegister"} element={<ItemRegister editable={false}/>}/>
-        <Route path={import.meta.env.BASE_URL + "/AddItem"} element={<ItemRegister editable={true}/>}/>
-        <Route path={import.meta.env.BASE_URL + "/ShoppingListPage/:id"} element={<ShoppingListPage title='title' items={[]}/>}/>
-        <Route path={import.meta.env.BASE_URL} element={<Home/>}/>
-      </Routes>
-    </div>
+      <Navbar title="GoCart" />
+      <div className="pt-24 h-screen ml-16 mr-16">
+        <Routes>
+          <Route path={import.meta.env.BASE_URL + '/Home'} element={<Home />} />
+          <Route path={import.meta.env.BASE_URL + '/ItemRegister'} element={<ItemRegister editable={false} />} />
+          <Route path={import.meta.env.BASE_URL + '/AddItem'} element={<ItemRegister editable={true} />} />
+          <Route
+            path={import.meta.env.BASE_URL + '/ShoppingListPage/:id'}
+            element={<ShoppingListPage title="title" items={[]} />}
+          />
+          <Route
+            path={import.meta.env.BASE_URL + '/ItemDetailsPage/:itemName'}
+            element={<ItemDetailsPage showArrowButtons={true} />}
+          />
+
+          <Route path={import.meta.env.BASE_URL} element={<Home />} />
+        </Routes>
+      </div>
     </Fragment>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- GoCart/frontend/src/components/ItemList.tsx
+++ GoCart/frontend/src/components/ItemList.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Item from './Item';
+import { Link } from 'react-router-dom';
 
 // Define the props for the ItemList component
 interface ItemListProps {
@@ -10,16 +11,18 @@ interface ItemListProps {
 
 const ItemList: React.FC<ItemListProps> = ({ items }) => {
   return (
-    <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-white p-2 rounded-2xl '>
+    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-white p-2 rounded-2xl ">
       {items.map((item, index) => (
-        <div className='card mx-2'>
-        <Item
-          key={index}
-          itemName={item.itemName}
-          increment={item.increment}
-          decrement={item.decrement}
-          quantity={item.quantity}
-        />
+        <div className="card mx-2">
+          <Link to={`/ItemDetailsPage/${item.itemName}`}>
+            <Item
+              key={index}
+              itemName={item.itemName}
+              increment={item.increment}
+              decrement={item.decrement}
+              quantity={item.quantity}
+            />
+          </Link>
         </div>
       ))}
     </div>
