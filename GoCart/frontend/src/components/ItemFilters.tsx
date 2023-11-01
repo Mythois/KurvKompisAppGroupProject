201c2f63cb7aa11257c08f9945f025bf64639894
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-11-01 11:36:16 +0100

    [#56] style: style button and searchbar

--- GoCart/frontend/src/components/ItemFilters.tsx
+++ GoCart/frontend/src/components/ItemFilters.tsx
@@ -12,17 +12,18 @@ function ItemFilters() {
   return (
     <div>
       {!isFilterVisible && (
-        <div className="bg-primary rounded-2xl p-3 flex gap-2 cursor-pointer text-white" onClick={handleFilterClick}>
-          <SlidersHorizontal size={24} color="white" />
-          <p>Filter</p>
-        </div>
+        // Filter button
+        <button className="btn flex gap-2 text-xl" onClick={handleFilterClick}>
+          <SlidersHorizontal size={30} color="white" />
+          Filter
+        </button>
       )}
 
       {isFilterVisible && (
-        <div className="border-2 border-primary rounded-2xl p-3">
+        <div className="border-2 border-primary rounded-lg p-2 text-xl">
           <div className="flex cursor-pointer border-b-2" onClick={handleFilterClick}>
             <X size={25} className="" />
-            <p>Close</p>
+            Close
           </div>
           {/* Render your filter options here */}
           <div>
