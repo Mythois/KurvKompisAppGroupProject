570c20c7418715ff4b551b87750f62bcf73a032d
Hannes Witt <hanneswitt@gmail.com>
2023-11-01 15:25:36 +0100

    [#6] Update resolvers.ts,typeDef.ts, Item.ts
    Update the files to correspond to the updated database model

--- GoCart/backend/src/models/typeDef.ts
+++ GoCart/backend/src/models/typeDef.ts
@@ -1,99 +1,41 @@
 import gql from 'graphql-tag'
 
 const typeDefs = gql`
-  #graphql
   type Item {
     _id: ID!
-    index: Int
     product_name_nb: String
     quantity: String
-    serving_size: String
-    packaging: [String]
-    packaging_tags: [String]
-    brands: [String]
-    brands_tags: [String]
     categories: [String]
-    categories_tags: [String]
-    labels: [String]
-    labels_tags: [String]
-    countries: [String]
-    countries_tags: [String]
-    stores: [String]
-    stores_tags: [String]
-    manufacturing_places: [String]
-    manufacturing_places_tags: [String]
-    ingredients_text_nb: [String]
-    allergens: [String]
+    allergens: String
     allergens_tags: [String]
-    traces: [String]
-    traces_tags: [String]
     nutrition_data_per: String
     nutrition_data_prepared_per: String
-    energy_kj_value: String
+    energy_kj_value: Float
     energy_kj_unit: String
-    energy_kcal_value: String
+    energy_kcal_value: Float
     energy_kcal_unit: String
-    fat_value: String
+    fat_value: Float
     fat_unit: String
-    saturated_fat_value: String
+    saturated_fat_value: Float
     saturated_fat_unit: String
-    carbohydrates_value: String
+    carbohydrates_value: Float
     carbohydrates_unit: String
-    sugars_value: String
+    sugars_value: Float
     sugars_unit: String
-    fiber_value: String
+    fiber_value: Float
     fiber_unit: String
-    proteins_value: String
+    proteins_value: Float
     proteins_unit: String
-    salt_value: String
+    salt_value: Float
     salt_unit: String
-    sodium_value: String
+    sodium_value: Float
     sodium_unit: String
-    energy_value: String
+    energy_value: Float
     energy_unit: String
-    monounsaturated_fat_value: String
+    monounsaturated_fat_value: Float
     monounsaturated_fat_unit: String
-    polyunsaturated_fat_value: String
+    polyunsaturated_fat_value: Float
     polyunsaturated_fat_unit: String
-    food_groups: String
-    food_groups_tags: [String]
-    nova_groups: String
-    nova_groups_tags: [String]
-    nutriscore_grade: String
-    nutriscore_score: String
-    ecoscore_grade: String
-    ecoscore_score: String
-    ecoscore_data: EcoscoreData
-    data_sources: [String]
-  }
-
-  type EcoscoreData {
-    missing_key_data: String
-    agribalyse: Agribalyse
-    adjustments: Adjustments
-  }
-
-  type Agribalyse {
-    code: String
-  }
-
-  type Adjustments {
-    origins_of_ingredients: OriginAdjustment
-    packaging: PackagingAdjustment
-    production_system: ProductionAdjustment
-  }
-
-  type OriginAdjustment {
-    value: String
-  }
-
-  type PackagingAdjustment {
-    value: String
-    non_recyclable_and_non_biodegradable_materials: String
-  }
-
-  type ProductionAdjustment {
-    value: String
   }
 
   type Query {
--- GoCart/backend/src/resolvers.ts
+++ GoCart/backend/src/resolvers.ts
@@ -2,45 +2,54 @@ import { ObjectId } from 'mongodb'
 
 const resolvers = (db) => ({
   Query: {
+    // Resolver to retrieve an item by its ID
     item: async (_, { _id }) => {
       try {
-        console.log(db)
+        // Find and return the item with the specified ID from the database
         const item = await db.collection('Items').findOne({ _id: new ObjectId(_id) })
-        console.log(item)
         return item
       } catch (error) {
         throw new Error(`Error retrieving item: ${error.message}`)
       }
     },
+
+    // Resolver to retrieve an item by its name
     itemByName: async (_, { product_name_nb }) => {
       try {
+        // Find and return the item with the specified name from the database
         const item = await db.collection('Items').findOne({ product_name_nb })
         return item
       } catch (error) {
         throw new Error(`Error retrieving item by name: ${error.message}`)
       }
     },
+
+    // Resolver to retrieve a list of items with pagination
     items: async (_, { page, perPage }) => {
       const skip = (page - 1) * perPage
       const limit = perPage
       try {
+        // Find and return a list of items with pagination from the database
         const allProducts = await db.collection('Items').find().skip(skip).limit(limit).toArray()
         return allProducts
       } catch (error) {
         throw new Error(`Error retrieving items: ${error.message}`)
       }
     },
+
+    // Resolver to retrieve a list of items by store with pagination
     itemsByStore: async (_, { store, page, perPage }) => {
       const skip = (page - 1) * perPage
       const limit = perPage
       try {
+        // Find and return a list of items by store with pagination from the database
         const items = await db.collection('Items').find({ stores: store }).skip(skip).limit(limit).toArray()
         return items
       } catch (error) {
         throw new Error(`Error retrieving items by store: ${error.message}`)
       }
     },
-    // Add other query resolvers using the same approach
+    // You can add other query resolvers here for new schema fields
   },
   // Add resolvers for mutations and other query types as needed
 })
