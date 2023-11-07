09e3c216e5db664a1da857464ce0537d33de5b33
Åshild Føllesdal <amfolle@stud.ntnu.no>
2023-11-07 19:12:53 +0100

    bug: fix for merge
    When trying to merge with development
    most of the new changes dissaper

--- GoCart/frontend/src/pages/ItemRegister.tsx
+++ GoCart/frontend/src/pages/ItemRegister.tsx
@@ -8,8 +8,8 @@ import NavButton from '../components/NavButton'
 import FilterDropdown from '../components/FilterDropdown'
 import { useQuery } from '@apollo/client'
 import { SEARCH_PRODUCTS } from '../utils/queryFunctions/getProduct'
-import ArrowButton from '../components/ArrowButton'
 import SortButtons from '../components/SortButtons'
+import { ChevronDown, ChevronUp } from 'lucide-react'
 
 interface ItemRegisterProps {
   editable: boolean
@@ -23,18 +23,19 @@ function ItemRegister({ editable }: ItemRegisterProps) {
   const [filter, setFilter] = useState('') // Filter for product names
   const [selectedCategory, setSelectedCategory] = useState('') // Selected category for filtering
   const [sortDirection, setSortDirection] = useState('asc') // Sort direction for product list
+  const [perPage, setPerPage] = useState(40) // initial value is 40
 
   // Translations for category names
   const categoryTranslations: { [key: string]: string } = {
     'Fruit & Vegetables': 'Frukt & grønt',
     'Fish & Seafood': 'Fisk & skalldyr',
-    'Beverages': 'Drikke',
+    Beverages: 'Drikke',
     'Baked Goods and Biscuits': 'Bakevarer og kjeks',
-    'Meat': 'Kjøtt',
+    Meat: 'Kjøtt',
     'Chicken & Poultry': 'Kylling og fjærkre',
     'Dairy & Eggs': 'Meieri & egg',
     'Snacks & Sweets': 'Snacks & godteri',
-    'Cheese': 'Ost',
+    Cheese: 'Ost',
     'Spread & Breakfast': 'Pålegg & frokost',
   }
 
@@ -48,15 +49,15 @@ function ItemRegister({ editable }: ItemRegisterProps) {
   const handleSortAsc = () => {
     setSortDirection('asc')
   }
-  
+
   // Handle descending sort
   const handleSortDesc = () => {
-    setSortDirection('desc')   
+    setSortDirection('desc')
   }
 
   // Fetch product data from GraphQL using Apollo Client
   const { loading, error, data } = useQuery(SEARCH_PRODUCTS, {
-    variables: { page: 1, perPage: 10, category: selectedCategory, name: filter, sortDirection: sortDirection },
+    variables: { page: 1, perPage: perPage, category: selectedCategory, name: filter, sortDirection: sortDirection },
   })
 
   let products: Product[] = data ? data.searchProducts : []
@@ -78,7 +79,7 @@ function ItemRegister({ editable }: ItemRegisterProps) {
       }))
 
   return (
-    <div className="h-full flex flex-col justify-center">
+    <div className="h-full flex flex-col justify-center sm:p-2 lg:pl-8 lg:pr-8">
       {/* Render the Searchbar component with the filter callback */}
       <div className="grid sm:flex gap-2 bg-white mb-2">
         <Searchbar onFilter={(value: React.SetStateAction<string>) => setFilter(value)} />
@@ -98,10 +99,23 @@ function ItemRegister({ editable }: ItemRegisterProps) {
         )}
       </div>
 
-      <div className="flex justify-between mb-5">
-        <ArrowButton direction="left" index={0} />
-        <NavButton route="AddItemToRegister" title={'Add item to register'} />
-        <ArrowButton direction="right" index={0} />
+      <div className="flex justify-between mb-5 gap-2">
+        <NavButton route="AddItemToRegister" title={'Add product'} />
+        <div className="flex gap-2">
+          <button className="btn flex" onClick={() => setPerPage(perPage + 40)}>
+            <ChevronDown />
+            <p className="hidden sm:block">Show more</p>
+          </button>
+          <button
+            className="btn flex"
+            onClick={() => {
+              if (perPage > 40) setPerPage(perPage - 40)
+            }}
+          >
+            <p className="hidden sm:block">Show less</p>
+            <ChevronUp />
+          </button>
+        </div>
       </div>
     </div>
   )
