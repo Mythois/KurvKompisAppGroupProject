28e47c4abaa0b51e6ccf316b493ead65915c866a
Åshild Føllesdal <amfolle@stud.ntnu.no>
2023-11-07 17:23:25 +0100

    [#74] feat: sort after name

--- GoCart/frontend/src/utils/queryFunctions/getProduct.ts
+++ GoCart/frontend/src/utils/queryFunctions/getProduct.ts
@@ -1,8 +1,9 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client'
 
 export const SEARCH_PRODUCTS = gql`
-query Query($page: Int!, $perPage: Int!, $category: String, $name:String){
-    searchProducts(page: $page, perPage: $perPage, category: $category, name: $name){
-        name
+  query Query($page: Int!, $perPage: Int!, $category: String, $name: String, $sortDirection: String) {
+    searchProducts(page: $page, perPage: $perPage, category: $category, name: $name, sortDirection: $sortDirection) {
+      name
     }
-}`
+  }
+`
