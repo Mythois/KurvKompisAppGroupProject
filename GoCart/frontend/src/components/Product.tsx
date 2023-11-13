030bcbaea72106845452b538933bbb8f7478c1d3
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-11-13 15:16:26 +0100

    [#93] bug: fixed fully clickable cards
    Closes[#93]

--- GoCart/frontend/src/components/Product.tsx
+++ GoCart/frontend/src/components/Product.tsx
@@ -1,46 +1,57 @@
-// product component represents an product in an ItemList
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+// This component renders a product card with the product name and the ability to increment or decrement the quantity of the product
+// If the increment or decrement props are false, then the buttons to increment or decrement the quantity are not displayed
 
-// It accepts props for item name, id, increment, decrement, and quantity
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 interface ProductProps {
-  productName: string // The name or description of the item
-  productID: string // The id of the item
-  increment?: boolean // Whether to display the increment button
-  decrement?: boolean // Whether to display the decrement button
-  quantity?: boolean // Whether to display the quantity field
+  productName: string
+  productID: string
+  increment?: boolean
+  decrement?: boolean
+  quantity?: boolean
 }
 
 function Product({ productName, productID, increment, decrement, quantity }: ProductProps) {
   const [productQuantity, setProductQuantity] = useState<number>(0)
+  const navigate = useNavigate()
 
-  // Function to increment the product quantity
-  function incrementProduct() {
+  function incrementProduct(event: React.MouseEvent<HTMLButtonElement>) {
+    event.stopPropagation() // Stop the event from propagating to the card
     setProductQuantity(productQuantity + 1)
   }
 
-  // Function to decrement the product quantity
-  function decrementProduct() {
+  function decrementProduct(event: React.MouseEvent<HTMLButtonElement>) {
+    event.stopPropagation() // Stop the event from propagating to the card
     if (productQuantity > 0) {
       setProductQuantity(productQuantity - 1)
     }
   }
 
-  return (
-    <div className="card flex justify-between">
-      {/* Display the product id based on 'productID' prop */}
-      <Link to={`/productDetailsPage/${productID}`} className="text-lg font-semibold col-span-2">
-        {productName}
-      </Link>
+  function handleCardClick() {
+    navigate(`/productDetailsPage/${productID}`)
+  }
 
-      {/* Display the buttons if props is true */}
+  return (
+    <div className="card flex justify-between" onClick={handleCardClick}>
+      <div className="text-lg font-semibold col-span-2">{productName}</div>
       {decrement && quantity && increment && (
         <div className="flex h-max">
-          <button className="btn" onClick={decrementProduct}>
+          <button
+            className="btn"
+            onClick={(e) => {
+              decrementProduct(e)
+            }}
+          >
             -
           </button>
           <span className="text-xl p-2">{productQuantity}</span>
-          <button className="btn" onClick={incrementProduct}>
+          <button
+            className="btn"
+            onClick={(e) => {
+              incrementProduct(e)
+            }}
+          >
             +
           </button>
         </div>
