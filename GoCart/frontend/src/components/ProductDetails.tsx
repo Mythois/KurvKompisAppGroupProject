5069ac156f1b33aaff94ad218b10fa171df46513
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-11-17 09:53:13 +0100

    [#36] delete navbutton

--- GoCart/frontend/src/components/ProductDetails.tsx
+++ GoCart/frontend/src/components/ProductDetails.tsx
@@ -96,15 +96,11 @@ function ProductDetails({ productID }: ProductDetailsProps) {
             <strong>Categories:</strong>
             <ul>
               {product.category ? (
-<<<<<<< HEAD
-                product.category.map((categoryproduct, index) => <li key={index}>{categoryproduct.name}</li>)
-=======
                 <>
                   {product.category.map((categoryproduct, index) => (
                     <li key={index}>{categoryproduct.name}</li>
                   ))}
                 </>
->>>>>>> 4898d65 ([#101] move product details to popup)
               ) : (
                 <li>N/A</li>
               )}
