6437436de051af194137839e83d51c26434a0365
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-11-18 17:11:30 +0100

    [#36]style: style the form with input fields

--- GoCart/frontend/src/pages/AddCustomProduct.tsx
+++ GoCart/frontend/src/pages/AddCustomProduct.tsx
@@ -9,6 +9,16 @@ interface InputField {
 import React, { useState } from 'react'
 
 function AddCustomProduct() {
+  const [showCategories, setShowCategories] = useState(false)
+  const [showNutrition, setShowNutrition] = useState(false)
+  const [showOther, setShowOther] = useState(false)
+
+  function toggleInput(inputType: string) {
+    setShowCategories(inputType === 'categories')
+    setShowNutrition(inputType === 'nutrition')
+    setShowOther(inputType === 'other')
+  }
+
   const [productInfo, setProductInfo] = useState({
     name: '',
     category1: '',
@@ -40,126 +50,172 @@ function AddCustomProduct() {
   return (
     <div className="pt-8 contentCenter">
       <h1 className="text-2xl">Add product to database</h1>
-      <div className="grid gap-2 my-2 mb-4">
+      <div className="grid gap-1 my-2 mb-4">
+        {/* name input */}
         <input
           type="text"
-          placeholder="Name"
+          placeholder="Product name*"
           className="inputfield mb-4"
           value={productInfo.name}
           onChange={(e) => handleInputChange('name', e.target.value)}
           required
         />
 
-        {/* categories input */}
-        <h2 className="text-xl">Categories</h2>
-        <div className="grid grid-cols-2 gap-2 mb-4">
-          <input
-            type="text"
-            placeholder="Category 1"
-            className="inputfield"
-            value={productInfo.category1}
-            onChange={(e) => handleInputChange('category1', e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Category 2"
-            className="inputfield"
-            value={productInfo.category2}
-            onChange={(e) => handleInputChange('category2', e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Category 3"
-            className="inputfield"
-            value={productInfo.category3}
-            onChange={(e) => handleInputChange('category3', e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Category 4"
-            className="inputfield"
-            value={productInfo.category4}
-            onChange={(e) => handleInputChange('category4', e.target.value)}
-          />
+        {/* product info input */}
+        <div className="grid gap-2 ">
+          {/* categories input */}
+          <button
+            className={`text-left ${showCategories ? 'btnClicked' : 'btn'}`}
+            onClick={() => {
+              toggleInput('categories')
+              setShowCategories(!showCategories)
+            }}
+          >
+            {showCategories ? ' - ' : ' + '}
+            Add categories
+          </button>
+          <div>
+            {showCategories && (
+              <div className="grid grid-cols-2 gap-2 mb-4 mt-1">
+                <input
+                  type="text"
+                  placeholder="Category 1"
+                  className="inputfield"
+                  value={productInfo.category1}
+                  onChange={(e) => handleInputChange('category1', e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Category 2"
+                  className="inputfield"
+                  value={productInfo.category2}
+                  onChange={(e) => handleInputChange('category2', e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Category 3"
+                  className="inputfield"
+                  value={productInfo.category3}
+                  onChange={(e) => handleInputChange('category3', e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Category 4"
+                  className="inputfield"
+                  value={productInfo.category4}
+                  onChange={(e) => handleInputChange('category4', e.target.value)}
+                />
+              </div>
+            )}
+          </div>
+
+          {/* nutrition input */}
+          <button
+            className={`text-left ${showNutrition ? 'btnClicked' : 'btn'}`}
+            onClick={() => {
+              toggleInput('nutrition')
+              setShowNutrition(!showNutrition)
+            }}
+          >
+            {showNutrition ? ' - ' : ' + '}
+            Add nutrition
+          </button>
+          <div>
+            {showNutrition && (
+              <div className="grid grid-cols-2 gap-2 mb-4 mt-1">
+                <input
+                  type="number"
+                  placeholder="Calories (per 100g)"
+                  className="inputfield"
+                  value={productInfo.calories}
+                  onChange={(e) => handleInputChange('calories', e.target.value)}
+                  min="0"
+                />
+                <input
+                  type="number"
+                  placeholder="Fat (per 100g)"
+                  className="inputfield"
+                  value={productInfo.fat}
+                  onChange={(e) => handleInputChange('fat', e.target.value)}
+                  min="0"
+                />
+                <input
+                  type="number"
+                  placeholder="Carbohydrates (per 100g)"
+                  className="inputfield"
+                  value={productInfo.carbohydrates}
+                  onChange={(e) => handleInputChange('carbohydrates', e.target.value)}
+                  min="0"
+                />
+                <input
+                  type="number"
+                  placeholder="Proteins (per 100g)"
+                  className="inputfield"
+                  value={productInfo.proteins}
+                  onChange={(e) => handleInputChange('proteins', e.target.value)}
+                  min="0"
+                />
+                <input
+                  type="number"
+                  placeholder="Salt (per 100g)"
+                  className="inputfield"
+                  value={productInfo.salt}
+                  onChange={(e) => handleInputChange('salt', e.target.value)}
+                  min="0"
+                />
+                <input
+                  type="number"
+                  placeholder="Sugar (per 100g)"
+                  className="inputfield"
+                  value={productInfo.sugar}
+                  onChange={(e) => handleInputChange('sugar', e.target.value)}
+                  min="0"
+                />
+              </div>
+            )}
+          </div>
+
+          {/* ... other input fields */}
+
+          <button
+            className={`text-left ${showOther ? 'btnClicked' : 'btn'}`}
+            onClick={() => {
+              toggleInput('other')
+              setShowOther(!showOther)
+            }}
+          >
+            {showOther ? ' - ' : ' + '}
+            Add other info
+          </button>
+          <div>
+            {showOther && (
+              <div className="grid grid-cols-2 gap-2 mb-4 mt-1">
+                <input
+                  type="text"
+                  placeholder="Store"
+                  className="inputfield"
+                  value={productInfo.store}
+                  onChange={(e) => handleInputChange('store', e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Brand"
+                  className="inputfield"
+                  value={productInfo.brand}
+                  onChange={(e) => handleInputChange('brand', e.target.value)}
+                />
+                <textarea
+                  placeholder="Additional information"
+                  className="textarea col-span-2"
+                  value={productInfo.additionalInfo}
+                  onChange={(e) => handleInputChange('additionalInfo', e.target.value)}
+                />
+              </div>
+            )}
+          </div>
         </div>
 
-        {/* nutrition input */}
-        <h2 className="text-xl">Nutrition</h2>
-        <div className="grid grid-cols-2 gap-2 mb-4">
-          <input
-            type="number"
-            placeholder="Calories (per 100g)"
-            className="inputfield"
-            value={productInfo.calories}
-            onChange={(e) => handleInputChange('calories', e.target.value)}
-            min="0"
-          />
-          <input
-            type="number"
-            placeholder="Fat (per 100g)"
-            className="inputfield"
-            value={productInfo.fat}
-            onChange={(e) => handleInputChange('fat', e.target.value)}
-            min="0"
-          />
-          <input
-            type="number"
-            placeholder="Carbohydrates (per 100g)"
-            className="inputfield"
-            value={productInfo.carbohydrates}
-            onChange={(e) => handleInputChange('carbohydrates', e.target.value)}
-            min="0"
-          />
-          <input
-            type="number"
-            placeholder="Proteins (per 100g)"
-            className="inputfield"
-            value={productInfo.proteins}
-            onChange={(e) => handleInputChange('proteins', e.target.value)}
-            min="0"
-          />
-          <input
-            type="number"
-            placeholder="Salt (per 100g)"
-            className="inputfield"
-            value={productInfo.salt}
-            onChange={(e) => handleInputChange('salt', e.target.value)}
-            min="0"
-          />
-          <input
-            type="number"
-            placeholder="Sugar (per 100g)"
-            className="inputfield"
-            value={productInfo.sugar}
-            onChange={(e) => handleInputChange('sugar', e.target.value)}
-            min="0"
-          />
-        </div>
-
-        {/* ... other input fields */}
-        <h2 className="text-xl">Other information</h2>
-        <div className="grid grid-cols-2 gap-2">
-          <input
-            type="text"
-            placeholder="Store"
-            className="inputfield"
-            value={productInfo.store}
-            onChange={(e) => handleInputChange('store', e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Brand"
-            className="inputfield"
-            value={productInfo.brand}
-            onChange={(e) => handleInputChange('brand', e.target.value)}
-          />
-          <textarea
-            placeholder="Additional Info"
-            className="textarea col-span-2"
-            value={productInfo.additionalInfo}
-            onChange={(e) => handleInputChange('additionalInfo', e.target.value)}
-          />
-        </div>
+        {/* add product button */}
       </div>
       <button className="btn" onClick={handleAddItem}>
         Add Product
