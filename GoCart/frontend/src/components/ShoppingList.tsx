53c53960de0775238f99f897de1d93d0936bc0de
Runarannem <runarannem@gmail.com>
2023-11-21 19:23:48 +0100

    [#125] enhance: Add message when list is empty

--- GoCart/frontend/src/components/ShoppingList.tsx
+++ GoCart/frontend/src/components/ShoppingList.tsx
@@ -13,16 +13,26 @@ interface ShoppingListProps {
 
 function ShoppingList({ title }: ShoppingListProps) {
   const shoppingListProducts: ProductProps[] = useReactiveVar(shoppingListProductsVar)
+  const isShoppingListEmpty = shoppingListProducts.length === 0
 
   return (
     <div className="h-full flex-col">
       <h1 className="mb-2 text-2xl font-bold uppercase text-center">{title}</h1>
-      <div className="h-3/4 overflow-auto mb-4 sm:mb-8">
-        <ProductList listView={true} products={shoppingListProducts} />
+      <div className="h-3/4 overflow-auto mb-4 sm:mb-8 relative">
+        {isShoppingListEmpty ? (
+          <div className="flex justify-center items-center h-full">
+            <div className="text-center">
+              <p>Handlelisten er for øyeblikket tom.</p>
+              <p>Trykk på "Legg til produkter" for å begynne på handlelisten din!</p>
+            </div>
+          </div>
+        ) : (
+          <ProductList listView={true} products={shoppingListProducts} />
+        )}
       </div>
       <div className="flex justify-center">
         <Link to={'/Addproduct'}>
-          <button className="btn">Add products</button>
+          <button className="btn">Legg til produkter</button>
         </Link>
       </div>
     </div>
