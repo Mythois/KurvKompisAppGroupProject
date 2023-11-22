c31bc635ca8fbf7a08184675d3d75f8ce30c791a
Åshild Føllesdal <amfolle@stud.ntnu.no>
2023-11-22 18:14:50 +0100

    [#59] update e2e test after merge

--- GoCart/frontend/cypress/e2e/Home.cy.ts
+++ GoCart/frontend/cypress/e2e/Home.cy.ts
@@ -17,7 +17,7 @@ describe('Home Page', () => {
     // Assert that the Add Product page is displayed
     cy.url().should('include', '/Addproduct')
     // Check for a spesifick product (first product in database)
-    cy.get('[data-testid="productid-654a23d4a609b87c60908e95"]').should('exist')
+    cy.get('[data-testid="product-654a23d4a609b87c60908e95"]').should('exist')
     // Check if increment button exist
     cy.get('[data-testid="increment-button-654a23d4a609b87c60908e95"]').should('exist')
     // Increment 3 times and decrement 1
@@ -30,7 +30,7 @@ describe('Home Page', () => {
     cy.get('[data-testid="back-to-shopping-list-button"]').click()
     cy.get('[data-testid="cypress-ShoppingList"]', { withinSubject: null }).should('exist')
     // Check if added product is dispalyed
-    cy.get('[data-testid="productid-654a23d4a609b87c60908e95"]').should('exist')
+    cy.get('[data-testid="product-654a23d4a609b87c60908e95"]').should('exist')
     // Check if displaying the right number
     cy.get('[data-testid="quantity-654a23d4a609b87c60908e95"]').contains('2')
   })
