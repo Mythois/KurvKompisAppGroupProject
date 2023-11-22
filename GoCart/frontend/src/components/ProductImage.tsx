89431f443418df1bf97c1362537ac25c6cd269c4
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-11-22 11:51:51 +0100

    [#99]style: style based on listview

--- GoCart/frontend/src/components/ProductImage.tsx
+++ GoCart/frontend/src/components/ProductImage.tsx
@@ -13,9 +13,9 @@ function ProductImage({ src, alt }: ProductImageProps) {
 
   productImage.onload = () => {
     if (productImage.width < productImage.height) {
-      setImageWidth('w-1/2')
+      setImageWidth('w-1/4 sm:w-1/2 md:w-1/3 lg:w-1/3')
     } else {
-      setImageWidth('w-3/4')
+      setImageWidth('w-1/3 sm:w-2/3 md:w-1/2 lg:w-2/4')
     }
   }
   return (
